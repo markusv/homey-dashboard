4533550486b8da056ad8fa268926272431c90893
Markus Voss <voss.m81@gmail.com>
2024-01-18 20:51:32 +0100

    fix: new heatpump id

--- src/components/Focus/constants.js
+++ src/components/Focus/constants.js
@@ -1,2 +1,2 @@
-export const HEATPUMP_DEVICE_ID = "aacca427-1cae-4d20-abd9-13e1e8bcda04";
+export const HEATPUMP_DEVICE_ID = "3114b5c8-c47e-4d9f-b83d-907e44e8557c";
 export const HEATPUMP_SET_TEMPERATURE = "f59e41b8-2334-40a3-ba76-7e6c4fa6cd09";
