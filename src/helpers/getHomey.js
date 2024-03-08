7b54c3295e1d85154a1cb77461aa70c41418bc47
Markus Voss <voss.m81@gmail.com>
2024-03-08 19:50:10 +0100

    fix: use local api

--- src/helpers/getHomey.js
+++ src/helpers/getHomey.js
@@ -1,4 +1,4 @@
-const AthomCloudAPI = require("homey-api/lib/AthomCloudAPI");
+import { HomeyAPI } from "homey-api";
 let homeyApiPromise = null;
 
 export const getHomey = async () => {
@@ -10,21 +10,9 @@ export const getHomey = async () => {
 };
 
 export const getHomeyAsync = async () => {
-  const cloudApi = new AthomCloudAPI({
-    clientId: "5a8d4ca6eb9f7a2c9d6ccf6d",
-    clientSecret: "e3ace394af9f615857ceaa61b053f966ddcfb12a",
-    redirectUrl: "https://homey-dashboard.vercel.app",
+  const homeyApi = await HomeyAPI.createLocalAPI({
+    address: "http://192.168.68.86",
+    token: process.env.REACT_APP_HOMEY_TOKEN ?? "",
   });
-  const loggedIn = await cloudApi.isLoggedIn();
-  if (!loggedIn) {
-    if (cloudApi.hasAuthorizationCode()) {
-      await cloudApi.authenticateWithAuthorizationCode();
-    } else {
-      window.location.href = cloudApi.getLoginUrl();
-      return;
-    }
-  }
-  const user = await cloudApi.getAuthenticatedUser();
-  const homey = await user.getFirstHomey();
-  return await homey.authenticate();
+  return homeyApi;
 };
