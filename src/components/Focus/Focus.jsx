3630b20c9115d6abeac65d5af45ebcbc8a4c90e8
Markus Voss <voss.m81@gmail.com>
2025-01-15 17:16:59 +0100

    fix: remove unused code

--- src/components/Focus/Focus.jsx
+++ src/components/Focus/Focus.jsx
@@ -11,44 +11,10 @@ import { ErrorBoundary } from "../ErrorBoundary";
 import { Weather } from "../Weather/Weather";
 
 export const Focus = ({ onSetFocus }) => {
-  const [localTempValue, setLocaltempValue] = useState(0);
-  const [heatpumpSetTemperature] = useGetLogicVariable(
-    HEATPUMP_SET_TEMPERATURE
-  );
-  const debouncedLocalTempValue = useDebounce(localTempValue, 1000);
-
-  useEffect(() => {
-    if (!heatpumpSetTemperature?.value) return;
-    if (heatpumpSetTemperature?.value !== localTempValue) {
-      setLocaltempValue(heatpumpSetTemperature?.value);
-    }
-  }, [heatpumpSetTemperature]);
-
-  useEffect(() => {
-    if (!heatpumpSetTemperature?.value) return;
-    if (debouncedLocalTempValue !== heatpumpSetTemperature?.value) {
-      setLogicVariable(HEATPUMP_SET_TEMPERATURE, debouncedLocalTempValue);
-    }
-  }, [debouncedLocalTempValue]);
-
-  const onChange = (e) => {
-    setLocaltempValue(e.srcElement.value);
-  };
-
   return (
     <div className="focus-container">
       <div className="focus-left">
         <FocusTemperature />
-        <div>
-          <div className="temperature">{`Ønsket temperatur: ${localTempValue}`}</div>
-          {/*<SlRange
-            min={10}
-            max={30}
-            step={1}
-            value={localTempValue}
-            onSlChange={onChange}
-          />*/}
-        </div>
       </div>
       <div className="focus-right">
         <ErrorBoundary>
