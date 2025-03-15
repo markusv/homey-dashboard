fafc1f7850791191dd826b3aeaefd39499ab3ddb
Markus Voss <voss.m81@gmail.com>
2025-03-15 08:35:59 +0100

    new heatpump ids

--- src/components/Focus/Focus.jsx
+++ src/components/Focus/Focus.jsx
@@ -1,11 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./focus.css";
-import { useGetLogicVariable } from "../../helpers/useGetLogicVariable";
-import { HEATPUMP_SET_TEMPERATURE } from "./constants";
 import { FocusTemperature } from "./FocusTemperature";
-import { SlRange } from "@shoelace-style/shoelace/dist/react";
-import { useDebounce } from "../../helpers/useDebounce";
-import { setLogicVariable } from "../../helpers/setLogicVariable";
 import { UtilityPricesSmall } from "../UtilityPrices/UtilityPricesSmall";
 import { ErrorBoundary } from "../ErrorBoundary";
 import { Weather } from "../Weather/Weather";
--- src/components/Focus/constants.js
+++ src/components/Focus/constants.js
@@ -1,2 +1,1 @@
-export const HEATPUMP_DEVICE_ID = "3114b5c8-c47e-4d9f-b83d-907e44e8557c";
-export const HEATPUMP_SET_TEMPERATURE = "f59e41b8-2334-40a3-ba76-7e6c4fa6cd09";
+export const HEATPUMP_DEVICE_ID = "47b30639-edf8-4d59-b89b-4b4351325f75";
